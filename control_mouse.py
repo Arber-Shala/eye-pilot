b8bfe940b6dc404dd9ccfecac8179164f0d3e8c9
arber <arber@ualberta.ca>
2024-11-14 23:48:40 -0700

    can detect segments of the eye

--- control_mouse.py
+++ control_mouse.py
@@ -31,10 +31,14 @@ import pyglet
 mouse.click('left')
 print(mouse.get_position())
 
-# https://www.youtube.com/watch?v=VWUgkcX_KoY
+# https://pysource.com/2019/01/07/eye-detection-gaze-controlled-keyboard-with-python-and-opencv-p-1/
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") # load in dataset that contains important locations on a face, such as location of eyes
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
 while True:
     _, frame = cap.read()
@@ -45,7 +49,18 @@ while True:
         x1, y1 = face.left(), face.top()
         x2, y2 = face.right(), face.bottom()
         cv2.rectangle(frame, (x1,y1), (x2, y2), (0,255,0), 2)
-        print(face)
+        
+        # eye detection
+        landmarks = predictor(gray, face)
+        #print(landmarks.part(39)) # print coordinate of inner eye
+        left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        center_top = midpoint(landmarks.part(37), landmarks.part(38))
+        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+
+        horizontal_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2) # create a horizontal line across the eye
+        vertical_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
     cv2.imshow("Frame", frame)
     
     key = cv2.waitKey(1)
