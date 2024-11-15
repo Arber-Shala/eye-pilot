92056031764bcaa982935749f08db75fcd71ace5
arber <arber@ualberta.ca>
2024-11-14 23:24:06 -0700

    detected face

--- control_mouse.py
+++ control_mouse.py
@@ -31,12 +31,21 @@ import pyglet
 mouse.click('left')
 print(mouse.get_position())
 
+# https://www.youtube.com/watch?v=VWUgkcX_KoY
 cap = cv2.VideoCapture(0)
 
+detector = dlib.get_frontal_face_detector()
 
 while True:
     _, frame = cap.read()
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # turn video to grayscale to save computation
+    faces = detector(gray)
+    for face in faces:
+        x1, y1 = face.left(), face.top()
+        x2, y2 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x1,y1), (x2, y2), (0,255,0), 2)
+        print(face)
     cv2.imshow("Frame", frame)
     
     key = cv2.waitKey(1)
