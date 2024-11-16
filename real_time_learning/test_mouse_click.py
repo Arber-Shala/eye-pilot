2a872ff8ca9b6bfd1f4893256aede9cf0f9e6930
icansingh <ikjyot@ualberta.ca>
2024-11-15 18:13:15 -0700

    Update test_mouse_click.py

--- real_time_learning/test_mouse_click.py
+++ real_time_learning/test_mouse_click.py
@@ -1,5 +1,13 @@
 import hecatron
-import mouse
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+def func():
+    pass
+
+def click():
+    mouse.click(Button.left)
 
 board = hecatron.init_board(None, 38)
-hecatron.run_live_session(board, action_functions=[None, mouse.click('left')], num_actions=2, reference_channels=[0], filename="test_run_4")
+hecatron.run_live_session(board, action_functions=[func, click], num_actions=2, reference_channels=[0], filename="test_run_4")
