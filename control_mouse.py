26502920e2d28e0cd0841330ba31b61213c556c9
craiglobo1 <craiglobo1@gmail.com>
2024-11-15 17:10:52 -0700

    control_mouse.py

--- control_mouse.py
+++ control_mouse.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import dlib
 import mouse
+import time
 
 
 def get_available_cameras() :
@@ -22,13 +23,14 @@ def get_available_cameras() :
 def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
-def movementV2(middle, new_position):
+def movementV2(middle, new_position, dt):
+    speed = 4
     x1, y1 = middle
     x2, y2 = new_position
     x_movement = x1 - x2
     y_movement = y2 - y1
     if abs(x_movement) >= 20 or abs(y_movement) >= 20:
-        mouse.move(x_movement * 1.2, y_movement * 1.2, False, 0.2)
+        mouse.move(x_movement * speed * dt, y_movement * speed * dt, False, 0.2)
         # time.sleep(0.2)
 
 
@@ -41,7 +43,6 @@ if __name__ == "__main__":
     for devid, devname in get_available_cameras().items():
         print(f"{devid}: {devname}")
 
-
     while not choice.isdigit() or choice == "":
         choice = input("choose video device id: ")
     choice = int(choice)
@@ -51,11 +52,14 @@ if __name__ == "__main__":
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") # load in dataset that contains important locations on a face
 
     count = 0
+    dt = 0.06
     while True:
+        prev = time.time()
         
         _, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # turn video to grayscale to save computation
         faces = detector(gray)
+
         for face in faces:
             # face detection
             x1, y1 = face.left(), face.top()
@@ -82,11 +86,13 @@ if __name__ == "__main__":
             if(count == 0):
                 middle = avg_point #center_top
                 count += 1
-            print(center_top)
-            print(middle)
-            movementV2(middle, avg_point)  # center_top
 
 
+            movementV2(middle, avg_point, dt)  # center_top
+
+
+
+        dt = time.time()-prev
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1)
         if key == 27: # if the esc key is pressed
