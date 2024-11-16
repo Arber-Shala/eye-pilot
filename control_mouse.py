4c93ca9d4c5a563c23475a7a00b4b0d4b5e77722
Arber <arber@ualberta.ca>
2024-11-15 17:11:38 -0700

    saving changes

--- control_mouse.py
+++ control_mouse.py
@@ -25,13 +25,13 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 # install the mouse package
-install("mouse")
-install("opencv-python==4.10.0.84")
-install("numpy")
-install("cmake")
-install("dlib")
-install("pyglet")
-install("pygrabber")
+# install("mouse")
+# install("opencv-python==4.10.0.84")
+# install("numpy")
+# install("cmake")
+# install("dlib")
+# install("pyglet")
+# install("pygrabber")
 
 
 # import packages
@@ -47,10 +47,19 @@ def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
 def movementV2(middle, new_position):
+    deadzone = [] # square in the callibrated center where the mouse does not move
+    
     x1, y1 = middle
     x2, y2 = new_position
+    print(x1, x2, y1, y2)
+    # x_movement = x1 - x2
+    # y_movement = y2 - y1
+
     x_movement = x1 - x2
-    y_movement = y2 - y1
+    y_movement = y1 - y2
+
+    print("x_movement", x_movement)
+    print("y_movement", y_movement)
     if abs(x_movement) >= 20 or abs(y_movement) >= 20:
         mouse.move(x_movement * 1.2, y_movement * 1.2, False, 0.2)
         # time.sleep(0.2)
@@ -103,11 +112,17 @@ if __name__ == "__main__":
             horizontal_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2) # create a horizontal line across the nose
             vertical_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
             # make the starting position of the nose as the reference for all mouse movements
-            if(count == 0):
-                middle = avg_point #center_top
-                count += 1
-            print(center_top)
-            print(middle)
+            # if(count == 0):
+            #     middle = avg_point #center_top
+            #     count += 1
+            #https://stackoverflow.com/questions/9734821/how-to-find-the-center-coordinate-of-rectangle
+            middle_x = int((x1 + x2) / 2)
+            middle_y = int((y1 + y2) / 2)
+            middle = (middle_x, middle_y)
+            print("middle:", middle)
+            # draw a circle where the center point is
+            # https://stackoverflow.com/questions/49799057/how-to-draw-a-point-in-an-image-using-given-co-ordinate-with-python-opencv
+            # cv2.circle(frame, (middle_x,middle_y), radius=0, color=(0, 0, 255), thickness=-1)
             movementV2(middle, avg_point)  # center_top
 
 
@@ -118,55 +133,3 @@ if __name__ == "__main__":
 
     cap.release()
     cv2.destroyAllWindows()
-
-
-# while True:
-#     ret, frame = cap.read()
-#     roi = frame[269: 795, 537:1416]
-#     rows, cols, _ = roi.shape
-#     gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-#     gray_roi = cv2.GaussianBlur(gray_roi, (7,7), 0)
-
-#     _, threshold = cv2.threshold(gray_roi, 5, 255, cv2.THRESH_BINARY)
-#     contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-#     for contour in contours:
-#         (x, y, w, h) = cv2.boundingRect(contour)
-#         cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#         cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
-#         cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
-#         break
-#     cv2.imshow("Frame", frame)
-#     key = cv2.waitKey(1)
-#     if key == 27: # if the esc key is pressed
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-
-# cascPath = sys.argv[1]
-# noseCascade = cv2.CascadeClassifier()
-
-# video_capture = cv2.VideoCapture(0)
-
-# while True:
-#     # Capture frame-by-frame
-#     ret, frame = video_capture.read()
-
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-#     nose = noseCascade.detectMultiScale(gray, 1.3, 5)
-#     # Draw a rectangle around the nose
-#     for (x, y, w, h) in nose:
-#         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#     # Display the resulting frame
-#     cv2.imshow('Video', frame)
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# # When everything is done, release the capture
-# video_capture.release()
-# cv2.destroyAllWindows()
