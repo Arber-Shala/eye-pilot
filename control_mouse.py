69254b75e32a4afa6fd1ad01ed00daae96b97c66
Arber <arber@ualberta.ca>
2024-11-16 18:25:28 -0700

    initial commit

--- control_mouse.py
+++ control_mouse.py
@@ -6,6 +6,9 @@ import dlib
 import mouse
 import time
 import math
+import pyautogui 
+from tkinter import *
+from tkinter import colorchooser, ttk
 
 def get_available_cameras() :
 
@@ -43,8 +46,78 @@ def movementV2(middle, new_position, dt):
     
 #     mouse.move(per_x*screen_width, per_y*screen_height, True, 0.1)
 
+def paint():
+    # importing pyautogui 
+    screenWidth, screenHeight = pyautogui.size() 
+    print(screenWidth)
+    
+class paint:
+    def __init__(self, master):
+        self.master = master
+        self.color_fg = 'Black'
+        self.color_bg = 'white'
+        self.old_x = None
+        self.old_y = None
+        self.pen_width = 5
+        self.drawWidgets()
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
 
-if __name__ == "__main__":    
+    def paint(self, e):
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, e.x, e.y, width = self.pen_width, fill = self.color_fg, capstyle='round', smooth = True)
+        self.old_x = e.x
+        self.old_y = e.y
+
+    def reset(self, e):
+        self.old_x = None
+        self.old_y = None
+    
+    def changedW(self, width):
+        self.pen_width = width
+    
+    def clearcanvas(self):
+        self.c.delete(ALL)
+    
+    def change_fg(self):
+        self.color_fg = colorchooser.askcolor(color=self.color_fg)[1]
+    
+    def change_bg(self):
+        self.color_bg = colorchooser.askcolor(color=self.color_bg)[1]
+        self.c['bg'] = self.color_bg
+
+    def drawWidgets(self):
+        self.controls = Frame(self.master, padx=5, pady=5)
+        textpw = Label(self.controls, text='Pen Width', font='Georgia 16')
+        textpw.grid(row=0, column=0)
+        self.slider = ttk.Scale(self.controls, from_=5, to=100, command=self.changedW, orient='vertical')
+        self.slider.set(self.pen_width)
+        self.slider.grid(row=0, column=1)
+        self.controls.pack(side="left")
+        self.c = Canvas(self.master, width=500, height=400, bg=self.color_bg)
+        self.c.pack(fill=BOTH, expand=True)
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        optionmenu = Menu(menu)
+        menu.add_cascade(label='Menu', menu=optionmenu)
+        optionmenu.add_command(label='Brush Color', command=self.change_fg)
+        optionmenu.add_command(label='Background Color', command=self.change_bg)
+        optionmenu.add_command(label='Clear Canvas', command=self.clearcanvas)
+        optionmenu.add_command(label='Exit', command=self.master.destroy)    
+
+
+
+
+
+
+if __name__ == "__main__": 
+    # output paint application
+    win = Tk()
+    win.title("Paint App")
+    paint(win)
+    win.mainloop()
+
     print("Pick a device id from the list")
     print("device id: device name")
     choice = ""
@@ -123,8 +196,8 @@ if __name__ == "__main__":
             center_bottom = midpoint(landmarks.part(33), landmarks.part(33))
 
             # test rotations
-            rotZ = math.atan(landmarks.part(31).y/landmarks.part(31).x)
-            print("rotZ", rotZ)
+            # rotZ = math.atan(landmarks.part(31).y/landmarks.part(31).x)
+            # print("rotZ", rotZ)
 
             avg_point_x = int((left_point[0] + right_point[0] + center_top[0] + center_bottom[0]) / 4)
             avg_point_y = int((left_point[1] + right_point[1] + center_top[1] + center_bottom[1]) / 4)
@@ -146,7 +219,61 @@ if __name__ == "__main__":
             # https://stackoverflow.com/questions/49799057/how-to-draw-a-point-in-an-image-using-given-co-ordinate-with-python-opencv
             cv2.circle(frame, (middle[0],middle[1]), radius=0, color=(0, 0, 255), thickness=5)
 
-            movementV2(middle, avg_point, dt)  # center_top
+            # **************************************************************************************************************
+            # GET ROTATION OF FACE
+            # https://learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
+            size = frame.shape
+ 
+            #2D image points. If you change the image, you need to change vector
+            image_points = np.array([
+                            (landmarks.part(33).x, landmarks.part(33).y),     # Nose tip
+                            (landmarks.part(8).x, landmarks.part(8).y),     # Chin
+                            (landmarks.part(45).x, landmarks.part(45).y),     # Left eye left corner
+                            (landmarks.part(36).x, landmarks.part(36).y),     # Right eye right corne
+                            (landmarks.part(54).x, landmarks.part(54).y),     # Left Mouth corner
+                            (landmarks.part(48).x, landmarks.part(48).y)      # Right mouth corner
+                        ], dtype="double")
+ 
+            # 3D model points.
+            model_points = np.array([
+                            (0.0, 0.0, 0.0),             # Nose tip
+                            (0.0, -330.0, -65.0),        # Chin
+                            (-225.0, 170.0, -135.0),     # Left eye left corner
+                            (225.0, 170.0, -135.0),      # Right eye right corne
+                            (-150.0, -150.0, -125.0),    # Left Mouth corner
+                            (150.0, -150.0, -125.0)      # Right mouth corner
+ 
+                        ])
+            # Camera internals
+ 
+            focal_length = size[1]
+            center = (size[1]/2, size[0]/2)
+            camera_matrix = np.array(
+                                    [[focal_length, 0, center[0]],
+                                    [0, focal_length, center[1]],
+                                    [0, 0, 1]], dtype = "double"
+                                    )
+
+            
+            dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs) #, flags=cv2.SOLVEPNP_ITERATIVE)
+            print("rotation_vector", rotation_vector) # euler vector
+
+            # Project a 3D point (0, 0, 1000.0) onto the image plane.
+            # We use this to draw a line sticking out of the nose
+            
+            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+            
+            for p in image_points:
+                cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+            
+            p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+            p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+            
+            cv2.line(frame, p1, p2, (255,0,0), 2)
+            # ***************************************************************************************************************
+
+            movementV2(middle, avg_point, dt)  # movementV2(middle, avg_point, dt) # center_top
             # movementV2(middle, avg_point, dt)  # center_top
         
         dt = time.time()-prev
